a135dea118850e9dae1452b1fe7fd6afdcd03217
Mary Ann <maryannliu01@gmail.com>
2025-03-22 19:06:13 +1000

    fix minor issues

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.239.53.81:5001', // local
+  baseURL: '/api' ,
+
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
