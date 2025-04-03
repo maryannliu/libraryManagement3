8fcb8ad508165e3238f71b4865a8dd9d27d5d9e1
Mary Ann <maryannliu01@gmail.com>
2025-04-04 09:14:12 +1000

    added base URL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api' ,
+  //baseURL: '/api' ,
 
-  //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://13.239.53.81:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
